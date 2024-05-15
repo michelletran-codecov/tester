b4d221ca836b9e3df1bf9c232b387332a920656f
Michelle Tran <michelle.tran@sentry.io>
2024-05-14 20:14:55 -0400

    fix spacing

--- func.py
+++ func.py
@@ -2,8 +2,11 @@ def func():
     """"Test function to check if coverage is configured properly."""
     return True
 
+
 def untested():
     return "This is untested code"
 
-def blah_test_this():
-    return "blue blue"
+
+def more_untested():
+    return "hello world"
+
